ec9eab683d432aaa29347c8baad582becc166f06
darisnurhakim <74717650+darisnurhakim@users.noreply.github.com>
2022-02-03 09:53:15 +0700

    Update perhitungan round robin scheduling
    - Semua proses masuk ke dalam antrian dengan urutan dan perhitungan yang tepat
    - Penyelesaian proses akan hadir dalam update-an selanjutnya

--- round robin scheduling.c
+++ round robin scheduling.c
@@ -1,53 +1,87 @@
-#include<stdio.h>  
- 
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
 
-    int jmlProses, kuantum;
-    printf("Program Round Robin Scheduling\n");
-    printf("Masukkan jumlah proses: ");
+int main(){
+    // input
+    int jmlProses,kuantum;
+    
+    printf("Round Robin Schedulling\n");
+    printf("Masukkan jumlah proses yang akan dilakukan: ");
     scanf("%d",&jmlProses);
 
-    int datang[jmlProses-1], eksekusi[jmlProses-1], temp[jmlProses-1];
+    int datang[jmlProses-1], eksekusi[jmlProses-1];
     int i = 0;
-    while (i < jmlProses){
-        printf("\nDetail Proses[%d]\n", i);
-        printf("Waktu kedatangan: ");
-        scanf("%d",&datang[i]);
-        printf("Waktu eksekusi: ");
-        scanf("%d",&eksekusi[i]);
-        temp[i] = eksekusi[i];
-        i += 1;
-    }
 
-    printf("\nMasukkan waktu kuantum: ");
+    while (i < jmlProses){
+        printf("Waktu kedatangan proses ke %d: ",i);
+        scanf("%d",&datang[i]);
+        printf("Waktu eksekusi proses ke %d: ",i);
+        scanf("%d",&eksekusi[i]);
+        i++;
+    }
+    printf("Masukkan waktu kuantum: ");
     scanf("%d",&kuantum);
 
-    printf("\n| Proses\t| Waktu masuk\t| Waktu eksekusi\t| Waktu selesai\t| Waktu tunggu\t|");
-    
-    int cek = jmlProses;
-    int waktu = 0;
-    int flag = 0;
-    for (waktu = 0, i = 0; cek > 0;) {
-        if (temp[i] <= kuantum && temp[i] > 0) {
-            waktu += temp[i];
-            temp[i] = 0;
-            flag = 1;
+    //proses masuk antrian
+    int proses1 = 0, proses2 = 0, n = 1;
+    int urutan = 0;
+    int proses3 = 0, proses4 = 0, flag = 0, o = 0, q = 2;
+    int urutanproses[jmlProses];
+    int* waktuproses = (int*)calloc(jmlProses-1, sizeof(int));
+
+    for (int waktu = 0; datang[jmlProses-1] >= waktu; waktu++){
+        if (datang[proses1] == waktu){
+            printf("\n----------------------------------\n");
+            printf("Waktu : %d ms\n", waktu);
+            printf("Proses ke %d masuk ke antrian\n",proses1);
+            urutanproses[urutan] = urutan;
+            urutan++;
+            for (int k = 0; k < proses1 + 1; k++){
+                printf("P[%d]\t",urutanproses[k]);
+            }
+            printf("\n");
+            waktuproses[proses3] = waktu - (o * kuantum);
+            if ((waktuproses[proses3] >= eksekusi[proses3]) || (eksekusi[proses3] <= kuantum)){
+                waktuproses[proses3] = 0;
+                waktuproses[proses4] += waktu - (o * kuantum); 
+                if (waktuproses[proses4] >= q * kuantum){
+                    waktuproses[proses3] = waktuproses[proses4] - q * kuantum;
+                    waktuproses[proses4] = q * kuantum;
+                    q++;
+                    proses4++;  
+                }
+            }
+            else if (waktuproses[proses3] >= kuantum){
+                waktuproses[proses3] = kuantum;
+                o++;
+                waktuproses[proses3 + 1] = waktu - (o * kuantum);
+                proses3++;
+                }
+            for (int p = 0; p < jmlProses; p++){
+                printf("P[%d]: %d ms\t", p, waktuproses[p]);
+            }
+            proses1++;
         }
-        else if (temp[i] > 0){
-            waktu += kuantum;
-            temp[i] -= kuantum;
-        }
-        if (temp[i] == 0 && flag == 1){
-            printf("\n| P[%d]\t\t| %d\t\t| %d\t\t\t| %d\t\t| %d\t\t|",i,datang[i],eksekusi[i],waktu,waktu - eksekusi[i]);
-            cek -= 1;
-            flag = 0;
-        }
-        if (i == jmlProses - 1){
-            i = 0;
-        }
-        else if (datang[i+1] <= waktu){
-            i += 1;
+        else if ((kuantum * n) == waktu){
+            printf("\n----------------------------------\n");
+            printf("Waktu : %d ms\n", waktu);
+            proses2 = urutanproses[0];
+            printf("Proses ke %d diantrikan kembali\n",proses2);
+            for (int m = 0; m <= proses1; m++){
+                urutanproses[m] = urutanproses[m + 1];
+                if (urutanproses[m] == urutanproses[proses1]){
+                    urutanproses[m] = proses2;
+                }
+            }
+            for (int l = 0; l < proses1; l++){
+                printf("P[%d]\t",urutanproses[l]);
+            }
+            n++;
+            proses2++;
         }
     }
-    printf("\n\nProgram selesai!");
-}   
+
+    // proses penyelesaian
+    printf("\n\n--Semua proses sudah masuk antrian--");
+
+}
